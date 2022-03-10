6c34e5752ba22270ebeb45b5971d65fadec2463d
Ayush Thapliyal <ayuthapliyal12@gmail.com>
2022-03-10 15:48:09 +0530

    binary sort

---
+++ Bubble sort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+main()
+{
+	int a[5],i,j,t;
+	printf("enter any number");
+	for(i=0;i<5;++i)
+{
+	scanf("%d",&a[i]);
+}	
+	for(i=0;i<4;++i)
+	{
+		for(j=0;j<4-i;++j)
+		if (a[j]>a[j+1]);
+		{
+		
+			t=a[j];
+			a[j]=a[j+i];
+			a[j+1]=t;
+		}
+		
+	}
+	printf("output is \n");
+		for(i=0;i<5;++i)
+	{
+		printf("%d\n",a[i]);
+	}
+}
